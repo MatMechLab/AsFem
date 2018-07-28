fc09747a4a2fd1bc4435734bec2b4b1aecbdf932
walkandthinker <walkandthinker@gmail.com>
2018-07-28 14:34:40 +0200

    add 2d mesh

--- FE/Mesh.py
+++ FE/Mesh.py
@@ -51,6 +51,17 @@ class Mesh1D:
         else:
             sys.exit('*** Error: wrong side name')
 
+    def GetIthElCoords(self,e):
+        if e<1 or e>self.nElmts:
+            sys.exit('*** Error: e=%d is out of range(nElmts=)!!!'%(e,self.nElmts))
+        elConn=self.Conn[e-1,:]
+        return self.NodeCoords[elConn]
+
+    def GetIthElConn(self,e):
+        if e<1 or e>self.nElmts:
+            sys.exit('*** Error: e=%d is out of range(nElmts=)!!!'%(e,self.nElmts))
+        return self.Conn[e-1,:]
+
     
     def PlotMesh(self):
         y=np.zeros(self.nNodesPerElmts)
@@ -60,6 +71,74 @@ class Mesh1D:
             plt.plot(x,y,'-+')
         
         plt.show()
+
+#################################################
+### For 2D Mesh
+#################################################
+class Mesh2D:
+    def __init__(self,nx,ny,xmin,xmax,ymin,ymax,meshtype):
+        if nx<1:
+            sys.exit('*** Error: nx=%d is invalid!!!'%(nx))
+        self.Nx=nx 
+
+        if ny<1:
+            sys.exit('*** Error: ny=%d is invalid!!!'%(ny))
+        self.Ny=ny
+
+        if xmin>=xmax:
+            sys.exit('*** Error: xmin=%g >= xmax=%g !!!'%(xmin,xmax))
+        self.Xmin=xmin
+        self.Xmax=xmax
+
+        if ymin>=ymax:
+            sys.exit('*** Error: ymin=%g >= ymax=%g !!!'%(ymin,ymax))
+        self.Ymin=ymin
+        self.Ymax=ymax
+
+        if meshtype != 'quad4' or meshtype !='quad8' or meshtype !='quad9':
+            sys.exit('*** Error: unsupported mesh type !!!')
+        self.MeshType=meshtype
+    
+    def CreateMesh(self):
+        if self.MeshType=='quad4':
+            dx=(self.Xmax-self.Xmin)
+            dy=(self.Ymax-self.Ymin)
+
+            self.nElmts=self.Nx*self.Ny
+            self.nNodes=(self.Nx+1)*(self.Ny+1)
+            self.nNodesPerElmts=4
+
+            self.NodeCoords=np.zeros((self.nNodes,2))
+            self.Conn=np.zeros((self.nElmts,self.nNodesPerElmts),dtype=np.int)
+
+            for j in range(1,self.Ny+1+1):
+                for i in range(1,self.Nx+1+1):
+                    k=(j-1)*(self.Nx+1)+i
+                    self.NodeCoords[k-1,0]=self.Xmin+(i-1)*dx
+                    self.NodeCoords[k-1,1]=self.Ymin+(j-1)*dy
+            
+            for j in range(1,self.Ny+1):
+                for i in range(1,self.Nx+1):
+                    e=(j-1)*self.Nx+i
+                    i1=(j-1)*(self.Nx+1)+i
+                    i2=i1+1
+                    i3=i2+self.Nx+1
+                    i4=i3-1
+                    self.Conn[e-1,0]=i1
+                    self.Conn[e-1,1]=i2
+                    self.Conn[e-1,2]=i3
+                    self.Conn[e-1,3]=i4
+    
+
+    def PlotMesh(self):
+        for e in range(self.nElmts):
+            elConn=self.Conn[e,:]-1
+            x=self.NodeCoords[elConn,0]
+            y=self.NodeCoords[elConn,1]
+            plt.plot(x,y,'-b+')
+        plt.show()
+        
+
 
 
 
