4d5640e6e32256ec551bad44d55ce6d5f2ef5173
yangbai90 <walkandthinker@gmail.com>
2020-02-07 10:30:20 +0100

    add Element System

---
+++ include/ElmtSystem/ElmtType.h
@@ -0,0 +1,48 @@
+//****************************************************************
+//* This file is part of the AsFem framework
+//* A Simple Finite Element Method program (AsFem)
+//* All rights reserved, Yang Bai @ CopyRight 2019
+//* https://github.com/walkandthinker/AsFem
+//* Licensed under GNU GPLv3, please see LICENSE for details
+//* https://www.gnu.org/licenses/gpl-3.0.en.html
+//****************************************************************
+
+#ifndef ASFEM_ELMTTYPE_H
+#define ASFEM_ELMTTYPE_H
+
+enum class ElmtType{
+    NullElmt,
+    PoissonElmt,
+    DiffusionElmt,
+    WaveElmt,
+    CahnHilliardElmt,
+    TensorCahnHilliardElmt,
+    MechCahnHilliardElmt,
+    MechanicsElmt,
+    ThermalMechanicsElmt,
+    PhaseFieldFracElmt,
+    MieheFractureElmt,
+    BordenFractureElmt,
+    User1Elmt,
+    User2Elmt,
+    User3Elmt,
+    User4Elmt,
+    User5Elmt,
+    User6Elmt,
+    User7Elmt,
+    User8Elmt,
+    User9Elmt,
+    User10Elmt,
+    User11Elmt,
+    User12Elmt,
+    User13Elmt,
+    User14Elmt,
+    User15Elmt,
+    User16Elmt,
+    User17Elmt,
+    User18Elmt,
+    User19Elmt,
+    User20Elmt
+};
+
+#endif //ASFEM_ELMTTYPE_H
