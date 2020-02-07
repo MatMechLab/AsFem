fb623e5807d6f90f5966bdbe18cc1c08a263aee0
yangbai90 <walkandthinker@gmail.com>
2020-02-07 10:32:42 +0100

    add Initial condition system

---
+++ src/ICs/ICSystem.cpp
@@ -0,0 +1,14 @@
+//****************************************************************
+//* This file is part of the AsFem framework
+//* A Simple Finite Element Method program (AsFem)
+//* All rights reserved, Yang Bai @ CopyRight 2020
+//* https://github.com/walkandthinker/AsFem
+//* Licensed under GNU GPLv3, please see LICENSE for details
+//* https://www.gnu.org/licenses/gpl-3.0.en.html
+//****************************************************************
+
+#include "ICs/ICSystem.h"
+
+ICSystem::ICSystem(){
+    _nICBlocks=0;
+}
