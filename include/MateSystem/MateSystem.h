6fa9020034c55ccfa48c6d6a9813ff04e5e23680
walkandthinker <walkandthinker@gmail.com>
2020-03-04 21:43:24 +0100

    add the first user defined material for non-linear poisson equation test in 2d

--- include/MateSystem/MateSystem.h
+++ include/MateSystem/MateSystem.h
@@ -177,6 +177,18 @@ private:
                           const vector<double> &gpU,const vector<double> &gpV,
                           const vector<Vector3d> &gpGradU,const vector<Vector3d> &gpGradV,
                           vector<double> &gpHist,const vector<double> &gpHistOld);
+
+    //******************************************************
+    //*** For User-Defined-Material (umat) code
+    //******************************************************
+    void UserMaterial1(const int &nDim,const double &t,const double &dt,
+                    const vector<double> InputParams,
+                    const Vector3d &gpCoord,
+                    const vector<double> &gpU,const vector<double> &gpV,
+                    const vector<Vector3d> &gpGradU,const vector<Vector3d> &gpGradV,
+                    vector<double> &gpHist,const vector<double> &gpHistOld);
+
+                    
 public:
     //********************************************
     //*** material paramers to be used in uel
