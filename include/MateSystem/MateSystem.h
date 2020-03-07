3d417ea4a3919dffab1be1b6ea299fe1c983dd0c
walkandthinker <walkandthinker@gmail.com>
2020-03-07 20:58:53 +0100

    add umat2 for phase field fracture with stochastic youngs modulus

--- include/MateSystem/MateSystem.h
+++ include/MateSystem/MateSystem.h
@@ -197,6 +197,13 @@ private:
                     const vector<double> &gpU,const vector<double> &gpV,
                     const vector<Vector3d> &gpGradU,const vector<Vector3d> &gpGradV,
                     vector<double> &gpHist,const vector<double> &gpHistOld);
+    //----- for 2nd user material
+    void UserMaterial2(const int &nDim,const double &t,const double &dt,
+                    const vector<double> InputParams,
+                    const Vector3d &gpCoord,
+                    const vector<double> &gpU,const vector<double> &gpV,
+                    const vector<Vector3d> &gpGradU,const vector<Vector3d> &gpGradV,
+                    vector<double> &gpHist,const vector<double> &gpHistOld);
 
                     
 public:
