9f89106fc3616ae0da1ff852bef029faa0fc1ddf
yangbai90 <walkandthinker@gmail.com>
2020-03-11 12:37:34 +0100

    change github link & update year

--- src/InputSystem/ReadBCBlock.cpp
+++ src/InputSystem/ReadBCBlock.cpp
@@ -1,8 +1,7 @@
 //****************************************************************
 //* This file is part of the AsFem framework
-//* A Simple Finite Element Method program (AsFem)
-//* All rights reserved, Yang Bai @ CopyRight 2019
-//* https://github.com/walkandthinker/AsFem
+//* A Simple Finite Element Method program 2020
+//* https://github.com/yangbai90/AsFem.git
 //* Licensed under GNU GPLv3, please see LICENSE for details
 //* https://www.gnu.org/licenses/gpl-3.0.en.html
 //****************************************************************
