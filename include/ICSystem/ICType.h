297f955b2d8dbcacdc51b6a5f038198ce390ebd7
walkandthinker <walkandthinker@gmail.com>
2020-07-11 17:13:03 +0200

    add ICType

---
+++ include/ICSystem/ICType.h
@@ -0,0 +1,35 @@
+//****************************************************************
+//* This file is part of the AsFem framework
+//* A Simple Finite Element Method program (AsFem)
+//* All rights reserved, Yang Bai @ CopyRight 2020
+//* https://github.com/yangbai90/AsFem.git
+//* Licensed under GNU GPLv3, please see LICENSE for details
+//* https://www.gnu.org/licenses/gpl-3.0.en.html
+//****************************************************************
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++ Author : Yang Bai
+//+++ Date   : 2020.07.10
+//+++ Purpose: Define the initial condition type in AsFem
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#pragma once
+
+enum class ICType{
+    NULLIC,
+    CONSTIC,
+    RANDOMIC,
+    RECTANGLEIC,
+    CUBICIC,
+    CIRCLEIC,
+    SPHERICALIC,
+    USER1IC,
+    USER2IC,
+    USER3IC,
+    USER4IC,
+    USER5IC,
+    USER6IC,
+    USER7IC,
+    USER8IC,
+    USER9IC,
+    USER10IC
+};
