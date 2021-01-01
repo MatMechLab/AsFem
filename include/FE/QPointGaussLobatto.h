28b682a55dc6d64f74cd2c80edb9ffd790a76dac
yangbai90 <walkandthinker@gmail.com>
2021-01-01 10:41:48 +0100

    update CopyRight year to 2021

--- include/FE/QPointGaussLobatto.h
+++ include/FE/QPointGaussLobatto.h
@@ -1,7 +1,7 @@
 //****************************************************************
 //* This file is part of the AsFem framework
 //* A Simple Finite Element Method program (AsFem)
-//* All rights reserved, Yang Bai @ CopyRight 2020
+//* All rights reserved, Yang Bai @ CopyRight 2021
 //* https://github.com/yangbai90/AsFem.git
 //* Licensed under GNU GPLv3, please see LICENSE for details
 //* https://www.gnu.org/licenses/gpl-3.0.en.html
@@ -9,7 +9,7 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //+++ Author : Yang Bai
 //+++ Date   : 2020.07.12
-//+++ Purpose: implement the Gauss–Lobatto rule for the integration
+//+++ Purpose: implement the GaussâLobatto rule for the integration
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #pragma once
--- include/Mesh/MeshType.h
+++ include/Mesh/MeshType.h
@@ -1,7 +1,7 @@
 //****************************************************************
 //* This file is part of the AsFem framework
 //* A Simple Finite Element Method program (AsFem)
-//* All rights reserved, Yang Bai @ CopyRight 2020
+//* All rights reserved, Yang Bai @ CopyRight 2021
 //* https://github.com/yangbai90/AsFem.git
 //* Licensed under GNU GPLv3, please see LICENSE for details
 //* https://www.gnu.org/licenses/gpl-3.0.en.html
