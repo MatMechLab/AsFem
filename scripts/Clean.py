db2ccbeae39f26496d3850f81f99016f9dc29297
yangbai90 <walkandthinker@gmail.com>
2021-02-22 21:09:39 +0100

    update Clean script to remove csv file from postprocess

--- scripts/Clean.py
+++ scripts/Clean.py
@@ -12,14 +12,21 @@ import shutil
 currentdir=os.getcwd()
 print('We are in folder:%s\n'%(currentdir))
 
-ASFEM=0;cmakefolder=0;ideafolder=0;o=0;vtu=0;metafile=0
+ASFEM=0;cmakefolder=0;ideafolder=0;o=0;vtu=0;metafile=0;csvfile=0
 cmake=0
 for subdir,dirs,files in os.walk(currentdir):
     if ('external/eigen' in subdir) or ('.git' in subdir):
         continue
     #>>> clean files
     for file in files:
-        if ('.i' in file) or ('.cpp' in file) or ('.C' in file) or ('.c' in file and 'cmake_install.cmake' not in file) or ('.h' in file) or ('.hpp' in file) or ('.msh' in file) or ('.geo' in file) or ('.gmsh2' in file) or ('.inp' in file) or ('.py' in file) or ('.C' in file) or ('.txt' in file and 'CMakeCache.txt' not in file) or ('.tex' in file) or ('.jpg' in file) or ('.png' in file) or ('.pdf' in file) or ('.json' in file) or ('.f90' in file) or ('.f' in file) or ('.xlsx' in file):
+        if ('.csv' in file):
+            try:
+                csvfile+=1
+                removepath=subdir+'/'+file
+                os.remove(removepath)
+            except:
+                print('%s is not here'%(file))
+        elif ('.i' in file) or ('.cpp' in file) or ('.C' in file) or ('.c' in file and 'cmake_install.cmake' not in file) or ('.h' in file) or ('.hpp' in file) or ('.msh' in file) or ('.geo' in file) or ('.gmsh2' in file) or ('.inp' in file) or ('.py' in file) or ('.C' in file) or ('.txt' in file and 'CMakeCache.txt' not in file) or ('.tex' in file) or ('.jpg' in file) or ('.png' in file) or ('.pdf' in file) or ('.json' in file) or ('.f90' in file) or ('.f' in file) or ('.xlsx' in file):
             continue
         elif ('ASFEM' in file) or ('asfem' in file):
             try:
@@ -81,6 +88,7 @@ for subdir,dirs,files in os.walk(currentdir):
 
 print('Remove %4d ASFEM files!'%(ASFEM))
 print('Remove %4d vtu files!'%(vtu))
+print('Remove %4d csv files!'%(csvfile))
 print('Remove %4d .o files!'%(o))
 print('Remove %4d meta files!'%(metafile))
 print('Remove %4d .idea folder!'%(ideafolder))
