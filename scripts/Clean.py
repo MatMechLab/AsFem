36f10f5f6d93559a8e074cea5f083df36b4edb6c
yang bai <walkandthinker@gmail.com>
2021-04-03 10:14:12 +0200

    add gif and figures folder to the white list of Clean script

--- scripts/Clean.py
+++ scripts/Clean.py
@@ -15,7 +15,7 @@ print('We are in folder:%s\n'%(currentdir))
 ASFEM=0;cmakefolder=0;ideafolder=0;o=0;vtu=0;metafile=0;csvfile=0
 cmake=0
 for subdir,dirs,files in os.walk(currentdir):
-    if ('external/eigen' in subdir) or ('.git' in subdir):
+    if ('external/eigen' in subdir) or ('.git' in subdir) or ('figures' in subdir):
         continue
     #>>> clean files
     for file in files:
@@ -26,7 +26,7 @@ for subdir,dirs,files in os.walk(currentdir):
                 os.remove(removepath)
             except:
                 print('%s is not here'%(file))
-        elif ('.i' in file) or ('.cpp' in file) or ('.C' in file) or ('.c' in file and 'cmake_install.cmake' not in file) or ('.h' in file) or ('.hpp' in file) or ('.msh' in file) or ('.geo' in file) or ('.gmsh2' in file) or ('.inp' in file) or ('.py' in file) or ('.C' in file) or ('.txt' in file and 'CMakeCache.txt' not in file) or ('.tex' in file) or ('.jpg' in file) or ('.png' in file) or ('.pdf' in file) or ('.json' in file) or ('.f90' in file) or ('.f' in file) or ('.xlsx' in file):
+        elif ('.i' in file) or ('.cpp' in file) or ('.C' in file) or ('.c' in file and 'cmake_install.cmake' not in file) or ('.h' in file) or ('.hpp' in file) or ('.msh' in file) or ('.geo' in file) or ('.gmsh2' in file) or ('.inp' in file) or ('.py' in file) or ('.C' in file) or ('.txt' in file and 'CMakeCache.txt' not in file) or ('.tex' in file) or ('.jpg' in file) or ('.jpeg' in file) or ('.png' in file) or ('.gif' in file) or ('.pdf' in file) or ('.doc' in file) or ('.docx' in file) or ('.json' in file) or ('.f03' in file) or ('.f08' in file) or ('.f90' in file) or ('.f' in file) or ('.xlsx' in file):
             continue
         elif ('ASFEM' in file) or ('asfem' in file):
             try:
