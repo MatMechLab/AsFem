97f907f110646e0047d8c4622c2b669ddf83f7f1
yangbai90 <yangbai90@outlook.com>
2021-10-30 21:38:29 +0200

    add User1Material and step7

--- src/MateSystem/User1Material.cpp
+++ src/MateSystem/User1Material.cpp
@@ -29,4 +29,64 @@ void User1Material::ComputeMaterialProperties(const vector<double> &InputParams,
     //**************************************************************
     if(InputParams.size()||elmtinfo.dt||elmtsoln.gpU[0]||MateOld.GetScalarMate().size()||Mate.GetScalarMate().size()){}
 
+    if(InputParams.size()<3){
+        MessagePrinter::PrintErrorTxt("for user1material, you need 3 parameters, they are E, nu, and delta");
+        MessagePrinter::AsFem_Exit();
+    }
+
+    double E0,EE,nu,delta;
+
+    E0=InputParams[1-1];
+    nu=InputParams[2-1];
+    delta=InputParams[3-1];
+
+    double x,y;
+    x=elmtinfo.gpCoords(1);
+    y=elmtinfo.gpCoords(2);
+
+    EE=E0*(1.0+delta*sin(x*y));
+
+    RankTwoTensor I,GradU,Strain,Stress,DevStress;
+    RankFourTensor Cijkl;
+
+    I.SetToIdentity();
+
+    GradU.SetFromGradU(elmtsoln.gpGradU[1],elmtsoln.gpGradU[2]);
+    Strain=(GradU+GradU.Transpose())*0.5;
+
+    Cijkl.SetFromEandNu(EE,nu);
+
+    Stress=Cijkl.DoubleDot(Strain);
+
+    // required by element
+    Mate.Rank2Materials("stress")=Stress;
+    Mate.Rank2Materials("strain")=Strain;
+    Mate.Rank4Materials("jacobian")=Cijkl;
+
+    Mate.ScalarMaterials("MyE")=EE;
+
+    I.SetToIdentity();
+    DevStress=Stress-I*(Stress.Trace()/3.0);
+    Mate.ScalarMaterials("vonMises")=sqrt(1.5*DevStress.DoubleDot(DevStress));
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
