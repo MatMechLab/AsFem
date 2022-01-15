5ac013225d7e4061d281c988ce4fd0488fbed048
yangbai90 <yangbai90@outlook.com>
2022-01-15 22:40:37 +0100

    add picewise linear interpolation function and tests

---
+++ tests/interpolation/test.cpp
@@ -0,0 +1,42 @@
+//****************************************************************
+//* This file is part of the AsFem framework
+//* A Simple Finite Element Method program (AsFem)
+//* All rights reserved, Yang Bai/M3 Group @ CopyRight 2022
+//* https://github.com/M3Group/AsFem
+//* Licensed under GNU GPLv3, please see LICENSE for details
+//* https://www.gnu.org/licenses/gpl-3.0.en.html
+//****************************************************************
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++ Author : Yang Bai
+//+++ Date   : 2022.01.15
+//+++ Purpose: test cpp for the functionality of AsFem
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+#include "petsc.h"
+
+#include "Utils/MathFuns.h"
+
+int main(int args,char *argv[]){
+    PetscErrorCode ierr;
+    ierr=PetscInitialize(&args,&argv,NULL,NULL);if (ierr) return ierr;
+
+    vector<double> xs,ys,xi;
+    
+    xs.push_back(0.0);ys.push_back(0.0);
+    xs.push_back(1.0);ys.push_back(1.0);
+    xs.push_back(2.0);ys.push_back(-1.0);
+
+    xi.push_back(0.5);
+    xi.push_back(1.5);
+    xi.push_back(1.8);
+    xi.push_back(2.5);
+    xi.push_back(3.5);
+
+    for(const auto &it:xi){
+        cout<<"xi="<<it<<", interpolated value="<<PicewiseLinearInterpolation(xs,ys,it)<<endl;
+    }
+
+    ierr=PetscFinalize();CHKERRQ(ierr);
+    return ierr;
+}
