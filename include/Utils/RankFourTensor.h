f6b6b5f7df8228cfb3d8def2d74efd90b7dbdd49
yangbai90 <yangbai90@outlook.com>
2022-03-29 20:24:54 +0200

    small changes & document

--- include/Utils/RankFourTensor.h
+++ include/Utils/RankFourTensor.h
@@ -26,14 +26,26 @@
 
 using namespace std;
 
+/**
+ * forward declaration
+ */
 class RankTwoTensor;
 
+/**
+ * this class implement the general operators/calculations in Rank-4 tensor
+ */
 class RankFourTensor{
 public:
+    /**
+     * constructor for different purpose
+     */
     RankFourTensor();
     RankFourTensor(const double &val);
     RankFourTensor(const RankFourTensor &a);
     
+    /**
+     * enum for different initializing
+     */
     enum InitMethod{
         InitZero,
         InitIdentity,
@@ -43,6 +55,13 @@ public:
     };
     RankFourTensor(const InitMethod &method);
 
+    /**
+     * get i-k components for jacobian matrix from a rank-4 tensor
+     * @param i integer for row-index
+     * @param j integer for col-index
+     * @param grad_test gradient of the test function
+     * @param grad_trial gradient of the tril function
+     */
     inline double GetIKjlComponent(const int &i,const int &k,
                                    const Vector3d &grad_test,
                                    const Vector3d &grad_trial)const{
@@ -67,6 +86,13 @@ public:
     //*** For operator overload
     //********************************************
     // for index based access
+    /**
+     * () operator for rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param i i index
+     * @param j j index
+     * @param k k index
+     * @param l l index
+     */
     inline double operator()(const int &i,const int &j,const int &k,const int &l) const{
         if(i<1||i>3 || j<1||j>3 || k<1||k>3 || l<1||l>3){
             MessagePrinter::PrintErrorTxt("your i or j or k or l is out of range when you call a rank-4 tensor");
@@ -74,6 +100,13 @@ public:
         }
         return _vals[(((i-1)*_N+j-1)*_N+k-1)*_N+l-1];
     }
+    /**
+     * () operator for rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param i i index
+     * @param j j index
+     * @param k k index
+     * @param l l index
+     */
     inline double& operator()(const int &i,const int &j,const int &k,const int &l){
         if(i<1||i>3 || j<1||j>3 || k<1||k>3 || l<1||l>3){
             MessagePrinter::PrintErrorTxt("your i or j or k or l is out of range when you call a rank-4 tensor");
@@ -81,11 +114,24 @@ public:
         }
         return _vals[(((i-1)*_N+j-1)*_N+k-1)*_N+l-1];
     }
+    /**
+     * get the voigt component of a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param i i index
+     * @param j j index
+     */
     double VoigtIJcomponent(const int &i,const int &j) const;
     // for components based access
+    /**
+     * [] operator to get the component of a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param i i index
+     */
     inline double  operator[](const int &i) const{
         return _vals[i-1];
     }
+    /**
+     * [] operator to get the component of a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param i i index
+     */
     inline double& operator[](const int &i){
         return _vals[i-1];
     }
@@ -94,72 +140,141 @@ public:
         return _vals[i];
     }
     //*** for =
+    /**
+     * = operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side scalar
+     */
     inline RankFourTensor& operator=(const double &a){
         for(int i=0;i<_N4;++i) _vals[i]=a;
         return *this;
     }
+    /**
+     * = operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand rank-4 tensor
+     */
     inline RankFourTensor& operator=(const RankFourTensor &a){
         for(int i=0;i<_N4;++i) _vals[i]=a._vals[i];
         return *this;
     }
     //*** for +
+    /**
+     * + operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side scalar
+     */
     inline RankFourTensor operator+(const double &a) const{
         RankFourTensor temp(0.0);
         for(int i=0;i<_N4;++i) temp._vals[i]=_vals[i]+a;
         return temp;
     }
+    /**
+     * + operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand rank-4 tensor
+     */
     inline RankFourTensor operator+(const RankFourTensor &a) const{
         RankFourTensor temp(0.0);
         for(int i=0;i<_N4;++i) temp._vals[i]=_vals[i]+a._vals[i];
         return temp;
     }
     //**** for +=
+    /**
+     * += operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side scalar
+     */
     inline RankFourTensor& operator+=(const double &a){
         for(int i=0;i<_N4;++i) _vals[i]=_vals[i]+a;
         return *this;
     }
+    /**
+     * += operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side rank-4 tensor
+     */
     inline RankFourTensor& operator+=(const RankFourTensor &a){
         for(int i=0;i<_N4;++i) _vals[i]=_vals[i]+a._vals[i];
         return *this;
     }
     //*** for -
+    /**
+     * - operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side scalar
+     */
     inline RankFourTensor operator-(const double &a) const{
         RankFourTensor temp(0.0);
         for(int i=0;i<_N4;++i) temp._vals[i]=_vals[i]-a;
         return temp;
     }
+    /**
+     * -= operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side rank-4 tensor
+     */
     inline RankFourTensor operator-(const RankFourTensor &a) const{
         RankFourTensor temp(0.0);
         for(int i=0;i<_N4;++i) temp._vals[i]=_vals[i]-a._vals[i];
         return temp;
     }
     //**** for -=
+    /**
+     * -= operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side scalar
+     */
     inline RankFourTensor& operator-=(const double &a){
         for(int i=0;i<_N4;++i) _vals[i]=_vals[i]-a;
         return *this;
     }
+    /**
+     * -= operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side rank-4 tensor
+     */
     inline RankFourTensor& operator-=(const RankFourTensor &a){
         for(int i=0;i<_N4;++i) _vals[i]=_vals[i]-a._vals[i];
         return *this;
     }
     //*** for *
+    /**
+     * * operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side scalar
+     */
     inline RankFourTensor operator*(const double &a) const{
         RankFourTensor temp(0.0);
         for(int i=0;i<_N4;++i) temp._vals[i]=_vals[i]*a;
         return temp;
     }
+    /**
+     * * operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side ran-2 tensor
+     */
     RankFourTensor operator*(const RankTwoTensor &a) const;
-    // this must be put into cpp file, otherwise the compiler will complain the errors!!!
+    /**
+     * * operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param lhs left hand side scalar
+     * @param a right hand side rank-4 tensor
+     */
     friend RankFourTensor operator*(const double &lhs,const RankFourTensor &a);
+    /**
+     * * operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param lhs left hand side rank-2 tensor
+     * @param a right hand side rank-4 tensor
+     */
     friend RankFourTensor operator*(const RankTwoTensor &lhs,const RankFourTensor &a);
 
     //**** for *=
+    /**
+     * *= operator to a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$
+     * @param a right hand side scalar
+     */
     inline RankFourTensor& operator*=(const double &a){
         for(int i=0;i<_N4;++i) _vals[i]=_vals[i]*a;
         return *this;
     }
     //*** for double dot operator
+    /**
+     * double dot : between a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$ and rank-2 tensor
+     * @param a right hand side rank-2 tensor
+     */
     RankTwoTensor DoubleDot(const RankTwoTensor &a) const;
+    /**
+     * double dot : between a rank-4 tensor \f$\mathbb{C}_{ijkl}\f$ and rank-4 tensor
+     * @param a right hand side rank-2 tensor
+     */
     inline RankFourTensor DoubleDot(const RankFourTensor &a) const{
         // C_ijkl=A_ijmn*B_mnkl
         RankFourTensor temp(0.0);
@@ -179,22 +294,47 @@ public:
         return temp;
     }
     //*** for rotated rank-4 tensor
+    /**
+     * rotat the current a rank-4 tensor by a rank-2 rotation tensor
+     * @param rotate right hand side rank-2 tensor
+     */
     RankFourTensor Rotate(const RankTwoTensor &rotate) const;
     //*** for push forward and pull back operator
+    /**
+     * push current rank-4 tensor by a rank-2 rotation tensor F
+     * @param F right hand side rank-2 tensor
+     */
     RankFourTensor PushByF(const RankTwoTensor &F) const;
+    /**
+     * pushforward current rank-4 tensor by a rank-2 rotation tensor F
+     * @param F right hand side rank-2 tensor
+     */
     RankFourTensor PushForward(const RankTwoTensor &F) const;
     //*** for conjugate dot Cijkl=Aijml*Smk
+    /**
+     * conjugate dot between current rank-4 tensor and a rank-2 tensor
+     * @param a right hand side rank-2 tensor
+     */
     RankFourTensor ConjDot(const RankTwoTensor &a) const;
     //**********************************************
     //*** some setting functions
     //**********************************************
+    /**
+     * set current rank-4 tensor to 0
+     */
     inline void SetToZeros(){
         for(int i=0;i<_N4;i++) _vals[i]=0.0;
     }
+    /**
+     * set current rank-4 tensor to idendity
+     */
     inline void SetToIdentity(){
         SetToZeros();
         for(int i=1;i<=_N;++i) (*this)(i,i,i,i)=1.0;
     }
+    /**
+     * set current rank-4 tensor to rank-4 identity
+     */
     inline void SetToIdentity4(){
         // maps a rank2 tensor to itself(no symmetric consideriation here),i.e. Iden4:rank2=rank2
         SetToZeros();
@@ -208,6 +348,9 @@ public:
             }
         }
     }
+    /**
+     * set current rank-4 tensor to rank-4 transposed identity
+     */
     inline void SetIdentity4Transpose(){
         // maps a rank-2 tensor to its transpose, A^{T}=I4:A
         SetToZeros();
@@ -221,6 +364,9 @@ public:
             }
         }
     }
+    /**
+     * set current rank-4 tensor to symmetric identity rank-4 tensor
+     */
     inline void SetToIdentitySymmetric4(){
         // symmetric fourth-order tensor
         SetToZeros();
@@ -235,6 +381,9 @@ public:
             }
         }
     }
+    /**
+     * set current rank-4 tensor to random values
+     */
     inline void SetToRandom(){
         srand(time(0));
         for(int i=1;i<=_N;++i){
@@ -250,6 +399,11 @@ public:
     //**********************************************
     //*** some fill-in functions
     //**********************************************
+    /**
+     * eps operator in 2d
+     * @param i i index
+     * @param j j index
+     */
     inline int Eps(const int &i,const int &j) const{
         if(i==1&&j==2) {
             return 1;
@@ -259,6 +413,12 @@ public:
         }
         return 0;
     }
+    /**
+     * eps operator in 3d
+     * @param i i index
+     * @param j j index
+     * @param k k index
+     */
     inline int Eps(const int &i,const int &j,const int &k) const{
         if(i==1&&j>1&&k>1){
             return Eps(j-1,k-1);
@@ -272,15 +432,41 @@ public:
         return 0;
     }
     //*** fill method
+    /**
+     * set rank-4 tensor from lamme constant and shear modulus
+     * @param Lame first Lame constant
+     * @param G shear modulus
+     */
     void SetFromLameandG(const double &Lame,const double &G);
+    /**
+     * set rank-4 tensor from Youngs modulus and poisson ratio
+     * @param E Youngs modulus
+     * @param Nu Poisson ratio
+     */
     void SetFromEandNu(const double &E,const double &Nu);
+    /**
+     * set rank-4 tensor from bulk modulus and shear modulus
+     * @param E Bulk modulus
+     * @param Nu Shear modulus
+     */
     void SetFromKandG(const double &K,const double &G);
+    /**
+     * set rank-4 tensor from 9-component
+     * @param vec 9-component for rank-4 tensor
+     */
     void SetFromSymmetric9(const vector<double> &vec);
+    /**
+     * set rank-4 tensor to Orthotropic
+     * @param vec 9-component for rank-4 tensor
+     */
     void SetToOrthotropic(const vector<double> &vec);
 
     //*****************************************
     //*** Print rank-4 tensor
     //*****************************************
+    /**
+     * print out the rank-4 tensor
+     */
     inline void Print() const{
         for(int i=1;i<=_N;++i){
             for(int j=1;j<=_N;++j){
@@ -291,6 +477,9 @@ public:
         }
     }
     //*******************
+    /**
+     * print out the rank-4 tensor in voigt notation
+     */
     inline void PrintVoigt() const{
         // taken from: https://en.wikipedia.org/wiki/Linear_elasticity
         PetscPrintf(PETSC_COMM_WORLD,"*** %12.5e  %12.5e  %12.5e  %12.5e  %12.5e  %12.5e***\n",(*this)(1,1,1,1),(*this)(1,1,2,2),(*this)(1,1,3,3),(*this)(1,1,2,3),(*this)(1,1,1,3),(*this)(1,1,1,2));
